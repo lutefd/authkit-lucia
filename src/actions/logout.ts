20a8f273aab354a52a168d136d725f4340f5e2d7
Luis Dourado <lfaddourado@gmail.com>
2024-01-31 18:37:29 -0300

    refactor(auth): update logout action to invalidate session and redirect to login page

--- src/actions/logout.ts
+++ src/actions/logout.ts
@@ -1,10 +1,16 @@
 'use server';
 
-import { signOut } from '@/server/auth';
+import { lucia } from '@/server/auth';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 /**
  * Signs out the user from the system
  */
 export const logout = async () => {
-	await signOut();
+	const sessionId = cookies().get('auth_session');
+	await lucia.invalidateSession(sessionId?.value!);
+	cookies().delete('auth_session');
+	cookies().delete('userSession');
+	redirect('/auth/login');
 };
