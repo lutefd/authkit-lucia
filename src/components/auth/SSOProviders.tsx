7791501a13f7af0617d724868698c7a82818e22b
Luis Dourado <lfaddourado@gmail.com>
2024-01-31 19:49:12 -0300

    refactor(auth): SSOProviders component to remove unused code and update GitHub button link

--- src/components/auth/SSOProviders.tsx
+++ src/components/auth/SSOProviders.tsx
@@ -1,17 +1,10 @@
-'use client';
-import { Button, buttonVariants } from '../ui/button';
+import { buttonVariants } from '../ui/button';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
-import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
-import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
 function SSOProviders() {
-	const router = useRouter();
-	const onClick = async (provider: 'google' | 'github') => {
-		console.log('provider', provider);
-	};
 	return (
 		<div className="flex items-center w-full gap-x-2">
 			<Link
@@ -26,14 +19,18 @@ function SSOProviders() {
 			>
 				<FcGoogle className="h-5 w-5" />
 			</Link>
-			<Button
-				size={'lg'}
-				className="w-full"
-				variant={'outline'}
-				onClick={() => onClick('github')}
+			<Link
+				href={`/api/auth/github`}
+				className={cn(
+					buttonVariants({
+						variant: 'outline',
+						size: 'lg',
+					}),
+					'w-full'
+				)}
 			>
 				<FaGithub className="h-5 w-5" />
-			</Button>
+			</Link>
 		</div>
 	);
 }
