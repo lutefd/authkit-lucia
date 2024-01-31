9b946dc374fbee17a0da8050573d6f592d77ab21
Luis Dourado <lfaddourado@gmail.com>
2024-01-31 18:48:18 -0300

    refactor(auth): update SSOProviders component with new Google OAuth implementation

--- src/components/auth/SSOProviders.tsx
+++ src/components/auth/SSOProviders.tsx
@@ -1,25 +1,31 @@
 'use client';
-import { Button } from '../ui/button';
+import { Button, buttonVariants } from '../ui/button';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
-import { signIn } from '@/server/auth';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
-import { oauthLogin } from '@/actions/oauth';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 function SSOProviders() {
+	const router = useRouter();
 	const onClick = async (provider: 'google' | 'github') => {
-		await oauthLogin(provider);
+		console.log('provider', provider);
 	};
 	return (
 		<div className="flex items-center w-full gap-x-2">
-			<Button
-				size={'lg'}
-				className="w-full"
-				variant={'outline'}
-				onClick={() => onClick('google')}
+			<Link
+				href={`/api/auth/google`}
+				className={cn(
+					buttonVariants({
+						variant: 'outline',
+						size: 'lg',
+					}),
+					'w-full'
+				)}
 			>
 				<FcGoogle className="h-5 w-5" />
-			</Button>
+			</Link>
 			<Button
 				size={'lg'}
 				className="w-full"
