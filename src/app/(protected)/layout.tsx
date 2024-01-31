4ac3320687238d49732a88e7e3aa543646b58b2e
Luis Dourado <lfaddourado@gmail.com>
2024-01-31 18:44:18 -0300

    refactor(auth): change AuthLayout component to use the new session management

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,21 +1,18 @@
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
-import { auth } from '@/server/auth';
 import { redirect } from 'next/navigation';
-import SessionProvider from '@/components/providers/SessionProvider';
 import Navbar from './_components/Navbar';
+import { readSession } from '@/actions/session';
 
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
-	const session = await auth();
-	if (!session) {
+	const session = await readSession();
+	if (!session.user) {
 		redirect('/auth/login');
 	}
 	return (
-		<SessionProvider session={session}>
-			<div className="h-full w-full flex flex-col gap-y-10 items-center justify-center  bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-400 to bg-purple-800">
-				<Navbar />
-				{children}
-			</div>
-		</SessionProvider>
+		<div className="h-full w-full flex flex-col gap-y-10 items-center justify-center  bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-400 to bg-purple-800">
+			<Navbar user={session.user!} />
+			{children}
+		</div>
 	);
 };
 
